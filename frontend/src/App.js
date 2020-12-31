ab8df111dc6773aeaff19e9ff78e90bda85a2406
AudreyNogueira <audreycbnf@gmail.com>
2020-12-31 18:22:02 -0300

    adicionando

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Menu from './Components/Menu';
+import Footer from './Components/Footer';
+// import Card from './Components/Card';
+
+
+function App() {
+  return (
+    <>
+      <Menu />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
